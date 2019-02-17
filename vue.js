54a08679b2f9fdf92e885a96fac9a910f8602c98
iolugovoy <iolugovoy@gmail.com>
2019-02-17 21:09:48 +0300

    adduser page done

--- vue.js
+++ vue.js
@@ -1,59 +1,131 @@
+var UsersTable = {
+  template: '#table',
+  props: {
+    title: {
+      type: String,
+      default: 'Значение по умолчанию',
+    },
+    users: {
+      type: Array,
+      required: true,
+    }
+  },
+  methods: { 
+    removeItem: function(user) {
+      this.$emit('remove', user.id);
+    },
+    fullName: function(user){
+      return user.lastName + ' ' + user.firstName + ' ' + user.patronymic;
+    },
+  }
+};
+
 var vm = new Vue({
-	el: '#app',
-	data: function(){
+  el: '#app',
+  components: {
+    'users-table': UsersTable
+
+  },
+  data: function(){
     return {
       message: 'Hello Vue.js!',
       tooltip: 'Показать \/ скрыть список',
-      users: [
-        {
-          firstName: 'Иван',
-          lastName: 'Петров',
-          patronymic: 'Сергеевич',
-          img: 'https://via.placeholder.com/50x50'
-        },
-        {
-          firstName: 'Сергей',
-          lastName: 'Петров',
-          patronymic: 'Иванович',
-        },
-        {
-          firstName: 'Петр',
-          lastName: 'Иванов',
-          patronymic: 'Сергеевич',
-          img: 'https://via.placeholder.com/50x50'
-        },
-        {
-          firstName: 'Иван',
-          lastName: 'Сергеев',
-          patronymic: 'Петрович',
-          img: 'https://via.placeholder.com/50x50'
-        },
-        {
-          firstName: 'Иван',
-          lastName: 'Петров',
-          patronymic: 'Иванович',
-        }
-      ],
+      users: [],
       showTable: true,
-		}
-	},
-  methods: {
-  	toggleTable: function () {
-      this.showTable = !this.showTable;
-                                                                                                                  },
+    }
+  },
+  
   computed: {
-  	usersCount: function(){
-    	return this.users.length;
+    usersCount: function(){
+      return this.users.length;
     },
     buttonText: function() {
-      return this.showTable ? 'Скрыть' : 'Пьказать';
+      return this.showTable ? 'Скрыть' : 'Показать';
+    },
+    activeUsers: function () {
+      return this.users.filter(function (item) { 
+        return item.isActive === true;
+      })
     }
   },
   filters: {
-  	upperCase: function(value){
-    	if (!value) return '';
-    	value = value.toString();
-    	return value.toUpperCase();
+    upperCase: function(value){
+      if (!value) return '';
+      value = value.toString();
+      return value.toUpperCase();
     }
-  }
+  },
+  watch: {
+      usersCount: 'usersChanged'
+  },
+  mounted: function () {
+    this.loadUsers();
+  },
+  methods: {
+    change: function () {
+      this.showTable = !this.showTable;
+    },
+    fullName: function(user){
+      return user.lastName + ' ' + user.firstName + ' ' + user.patronymic;
+    },
+    usersChanged: function () {
+      console.log('User count changed');
+    },
+    loadUsers: function () {
+      this.users =[
+            {
+              id: "1",
+              isActive: true,
+              firstName: 'Иван',
+              lastName: 'Петров',
+              patronymic: 'Сергеевич',
+              img: 'https://via.placeholder.com/50x50'
+            },
+            {
+              id: "2",
+              isActive: true,
+              firstName: 'Сергей',
+              lastName: 'Петров',
+              patronymic: 'Иванович',
+            },
+            {
+              id: "3",
+              isActive: true,
+              firstName: 'Петр',
+              lastName: 'Иванов',
+              patronymic: 'Сергеевич',
+              img: 'https://via.placeholder.com/50x50'
+            },
+            {
+              id: "4",
+              isActive: true,
+              firstName: 'Иван',
+              lastName: 'Сергеев',
+              patronymic: 'Петрович',
+              img: 'https://via.placeholder.com/50x50'
+            },
+            {
+              id: "5",
+              isActive: true,
+              firstName: 'Иван',
+              lastName: 'Петров',
+              patronymic: 'Иванович',
+            },
+            {
+              id: "6",
+              isActive: false,
+              firstName: 'Иван',
+              lastName: 'Петров',
+              patronymic: 'Иванович',
+            }
+          ]
+    },
+    removeUser: function (id) {
+      console.log('user removed', id)
+      this.users = this.users.filter(function (item) {
+        return item.id !== id 
+      })
+    }
+  },
 });
+  
